4d3aa3386b3fa134d468f23016d3f268be9de402
xfrogcn <xfrogcn@163.com>
2020-09-16 18:33:41 +0800

    素材上传

---
+++ src/WXMPSDK/Dto/Response/WXMediaUploadResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WXMPSDK.Dto
+{
+    /// <summary>
+    /// 素材上传应答
+    /// </summary>
+    public class WXMediaUploadResponse: WXResponse
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("media_id")]
+        public string MediaId { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public long CreatedAt { get; set; }
+    }
+}
