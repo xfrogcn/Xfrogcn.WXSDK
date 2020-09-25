25d35395ef7a493f58b894248977caa284205ea4
xfrogcn <xfrogcn@163.com>
2020-09-25 15:49:25 +0800

    日期扩展

---
+++ src/WXMPSDK/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        private readonly static DateTime beginTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public static long ToWXTime(this DateTime dt)
+        {
+            return (long)Math.Round(dt.ToUniversalTime().Subtract(beginTime).TotalSeconds);
+        }
+
+        public static DateTime ToNetCoreTime(this long dt)
+        {
+            return beginTime.AddSeconds(dt).ToLocalTime();
+        }
+    }
+}
