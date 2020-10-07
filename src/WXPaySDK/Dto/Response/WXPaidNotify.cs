f24b51ef9208507abdc1d15f781cc1273f58f7ba
xfrogcn <xfrogcn@163.com>
2020-10-08 02:06:20 +0800

    支付通知

---
+++ src/WXPaySDK/Dto/Response/WXPaidNotify.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace WXPaySDK.Dto
+{
+    [Serializable]
+    [XmlRoot("xml")]
+    public class WXPaidNotify : WXPayResponseBase
+    {
+        [XmlElement("appid")]
+        public string AppId { get; set; }
+
+        [XmlElement("mch_id")]
+        public string MchId { get; set; }
+
+        [XmlElement("device_info")]
+        public string DeviceInfo { get; set; }
+
+        [XmlElement("sign_type")]
+        public string SignType { get; set; }
+
+        [XmlElement("openid")]
+        public string OpenId { get; set; }
+
+        [XmlElement("is_subscribe")]
+        public string IsSubscribe { get; set; }
+
+        [XmlElement("trade_type")]
+        public string TradeType { get; set; }
+
+        [XmlElement("bank_type")]
+        public string BankType { get; set; }
+
+        [XmlElement("total_fee")]
+        public int TotalFee { get; set; }
+
+        [XmlElement("settlement_total_fee")]
+        public int SettlementTotalFee { get; set; }
+
+        [XmlElement("fee_type")]
+        public string FeeType { get; set; }
+
+        [XmlElement("cash_fee")]
+        public int CashFee { get; set; }
+
+        [XmlElement("cash_fee_type")]
+        public string CashFeeType { get; set; }
+
+        [XmlElement("coupon_fee")]
+        public int CouponFee { get; set; }
+
+        [XmlElement("coupon_count")]
+        public int CouponCount { get; set; }
+
+        [XmlElement("transaction_id")]
+        public string TransactionId { get; set; }
+
+        [XmlElement("out_trade_no")]
+        public string OutTradeNo { get; set; }
+
+        [XmlElement("attach")]
+        public string Attach { get; set; }
+
+        [XmlElement("time_end")]
+        public string TimeEnd { get; set; }
+
+
+        public static WXPaidNotify FromXml(Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(WXPaidNotify));
+
+            return serializer.Deserialize(stream) as WXPaidNotify;
+        }
+    }
+}
---
+++ src/WXPaySDK/Dto/Response/WXPaidNotifyResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WXPaySDK.Dto
+{
+    [Serializable]
+    [XmlRoot("xml")]
+    public class WXPaidNotifyResponse
+    {
+        /// <summary>
+        /// SUCCESS/FAIL 此字段是通信标识，非交易标识，交易是否成功需要查看result_code来判断
+        /// </summary>
+        [XmlElement("return_code")]
+        public string ReturnCode { get; set; }
+
+        [XmlElement("return_msg")]
+        public string ReturnMsg { get; set; }
+
+        public virtual string ToXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            MemoryStream ms = new MemoryStream();
+            XmlWriter writer = XmlWriter.Create(ms, new XmlWriterSettings()
+            {
+                OmitXmlDeclaration = true,
+                NamespaceHandling = NamespaceHandling.OmitDuplicates
+            });
+            XmlSerializerNamespaces _namespaces = new XmlSerializerNamespaces(
+                    new XmlQualifiedName[] {
+                        new XmlQualifiedName(string.Empty, "")
+                 });
+            serializer.Serialize(writer, this, _namespaces);
+            ms.Position = 0;
+            TextReader reader = new StreamReader(ms);
+            return reader.ReadToEnd();
+        }
+    }
+}
