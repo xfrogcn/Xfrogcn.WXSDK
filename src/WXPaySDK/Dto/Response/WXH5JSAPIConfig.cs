de1705a0473fd9bce34cfc2e2403c784d803eeb7
xfrogcn <xfrogcn@163.com>
2020-10-09 23:48:13 +0800

    JSAPIConfig

---
+++ src/WXPaySDK/Dto/Response/WXH5JSAPIConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace WXPaySDK.Dto
+{
+    public class WXH5JSAPIConfig
+    {
+        [XmlElement("appId")]
+        [JsonPropertyName("appId")]
+        public string AppId { get; set; }
+
+        [XmlElement("timeStamp")]
+        [JsonPropertyName("timeStamp")]
+        public string TimeStamp { get; set; }
+
+        [XmlElement("nonceStr")]
+        [JsonPropertyName("nonceStr")]
+        public string NonceStr { get; set; }
+
+        [XmlElement("package")]
+        [JsonPropertyName("package")]
+        public string Package { get; set; }
+
+        [XmlElement("signType")]
+        [JsonPropertyName("signType")]
+        public string SignType { get; set; }
+
+        [XmlElement("paySign")]
+        [JsonPropertyName("paySign")]
+        public string PaySign { get; set; }
+    }
+}
